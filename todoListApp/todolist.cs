a5af621122b985a8f798ab014769f240579671a9
howlieT <misha.anker@gmail.com>
2025-08-21 20:14:17 +0100

    Classes in place

--- todoListApp/todolist.cs
+++ todoListApp/todolist.cs
@@ -1,18 +1,59 @@
-namespace todolist;
+using system;
+using system.collections.generic;
+using system.linq;
 
-public class storeTasks
+namespace ToDoList;
+
+public class TaskItem
 {
-    class Task
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsCompleted { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string DueDate { get; set; }
+    public string Priority { get; set; }
+    public bool IsCompleted { get; set; }
 
-        public Task(string title, string description)
+    public TaskItem(string title, string description, string dueDate, string priority)
+    {
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+        Priority = priority;
+        IsCompleted = false;
+    }
+}
+
+public class TaskStore
+{
+    public List<TaskItem> TaskList { get; set; }
+
+    public TaskStore()
+    {
+        TaskList = new List<TaskItem>();
+    }
+
+    public void AddTask(string title, string description, string dueDate, string priority)
+    {
+        var task = new TaskItem(title, description, dueDate, priority);
+        TaskList.Add(task);
+    }
+
+    public List<TaskItem> GetCompletedTasks()
+    {
+        return TaskList.Where(t => t.IsCompleted).ToList();
+    }
+
+    public List<TaskItem> GetIncompleteTasks()
+    {
+        return TaskList.Where(t => !t.IsCompleted).ToList();
+    }
+
+    public void MarkTaskComplete(string title)
+    {
+        var task = TaskList.FirstOrDefault(t => t.Title == title);
+        if (task != null)
         {
-            Title = title;
-            Description = description;
-            IsCompleted = false;
+            task.IsCompleted = true;
         }
     }
 }
+
